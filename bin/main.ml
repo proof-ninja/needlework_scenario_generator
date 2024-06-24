e5417550372b500edfde4cf4c2f462392310a3ae
Yoshihiro Imai <y.imai@proof-ninja.co.jp>
2024-06-24 22:05:35 +0900

    feat : :goal_net: filename as a command line parameter

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,13 @@
 open Needlework_scenario_generator
 
+let get_filename () =
+  if Array.length Sys.argv = 1 then begin
+      Log.error "Usage: %s <filename>.xml\n" Sys.argv.(0); exit 1
+    end else Sys.argv.(1)
 
 
 let () =
-  let filename = Sys.argv.(1) in
+  let filename = get_filename () in
   let xml = Xml.parse_file filename in
   let (book, rules) = Reader.read xml in
   Log.debug "book is:\n  %s\n" (AddressBook.dump book);
