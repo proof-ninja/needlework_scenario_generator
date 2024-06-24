73e6fb44451179d38f1a41b1af3f1d2c01e152e2
Yoshihiro Imai <y.imai@proof-ninja.co.jp>
2024-06-25 06:14:30 +0900

    feat: :arrow_down: support ocaml under version 4.14

--- lib/common.ml
+++ lib/common.ml
@@ -10,3 +10,33 @@ module SeqMonad = struct
 end
 
 module Log = Dolog.Log
+
+module Seq = struct
+  include Seq
+
+  let rec mapi_aux f i xs () =
+    match xs() with
+    | Nil ->
+       Nil
+    | Cons (x, xs) ->
+       Cons (f i x, mapi_aux f (i+1) xs)
+
+  let[@inline] mapi f xs =
+    mapi_aux f 0 xs
+
+  let rec take_aux n xs =
+    if n = 0 then
+      empty
+    else
+      fun () ->
+      match xs() with
+      | Nil ->
+         Nil
+      | Cons (x, xs) ->
+         Cons (x, take_aux (n-1) xs)
+
+  let take n xs =
+    if n < 0 then invalid_arg "Seq.take";
+    take_aux n xs
+
+end
